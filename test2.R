6ce8998d6e32a3c4667d12c5f2e3efe7ad98591c
ColeShota <shota.japarashvili.1@iliauni.edu.ge>
2022-11-17 10:18:20 +0100

    aaaa

--- test2.R
+++ test2.R
@@ -1,3 +1,4 @@
 #load packages
 #before it will apper in GIT window, you have to save it. After doing so things that you already saved will appera in read
 library(tidyverse)
+#now with the branches, if we want to commite (combine) branches we have to push tham
