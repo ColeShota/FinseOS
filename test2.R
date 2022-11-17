c04adb234be3e84d84638ca37a970074c9ec4f81
ColeShota <shota.japarashvili.1@iliauni.edu.ge>
2022-11-17 11:26:04 +0100

    ann

--- test2.R
+++ test2.R
@@ -2,3 +2,4 @@
 #before it will apper in GIT window, you have to save it. After doing so things that you already saved will appera in read
 library(tidyverse)
 #now with the branches, if we want to commite (combine) branches we have to push tham
+Hello
