60a7b5464ac594fb7450e19cc0f54f334273db06
ColeShota <shota.japarashvili.1@iliauni.edu.ge>
2022-11-17 09:15:31 +0100

    initiate commit

--- Untitled,1.R
+++ Untitled,1.R
@@ -1,9 +1,11 @@
-install.packages("usethis")
 
 library(usethis)
 use_git_config(
   user.name = "ColeShota",
   user.email = "shota.japarashvili.1@iliauni.edu.ge"
 )
+
 usethis::create_github_token()
-gitcreds::gitcreds_set("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")
+gitcreds::gitcreds_set("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7")
+
+use_github()
